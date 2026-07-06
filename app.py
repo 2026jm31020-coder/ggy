f48d9d32f3c8090e3ee49e13ba14a877812c7a1b
2026jm31020-coder <2026jm31020@jeonminhs.djsch.kr>
2026-07-06 23:53:37 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import warnings
 from sklearn.ensemble import RandomForestClassifier
-import plotly.express as px
 
 # 경고 메시지 끄기
 warnings.filterwarnings('ignore', category=UserWarning)
